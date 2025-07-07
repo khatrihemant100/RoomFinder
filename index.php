8630f6caf0baeb5a5f988ee1a5c6e97036ade9e3
123pk123 <240036pt@yse-c.net>
2025-07-07 10:06:30 +0900

    delete comment

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RoomFinder - Find Your Perfect Room</title>
-    <!-- नेपाली नोट: Google Fonts जोडिएको छ -->
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
